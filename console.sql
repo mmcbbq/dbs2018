e50181b8cb97299f31b74ecd23f16b1d2a6351da
M.Martinez <m.martinez@bbq.de>
2023-08-01 08:16:18 +0200

    add comments

--- console.sql
+++ console.sql
@@ -1,37 +1,44 @@
 create database dbs2018;
 use dbs2018;
+
+/* 20 Kunden */
+
 create table Kunde(
-    Kunde_ID int PRIMARY KEY auto_increment,
-    Firma varchar(255),
-    Strasse varchar(255),
-    Plz varchar(255),
-    Ort varchar(255)
+                      Kunde_ID int PRIMARY KEY auto_increment,
+                      Firma varchar(255),
+                      Strasse varchar(255),
+                      Plz varchar(255),
+                      Ort varchar(255)
 );
+/* 10 Hersteller */
+
+create table Hersteller(
+                           Hersteller_ID int PRIMARY KEY auto_increment ,
+                           Firma varchar(255)
+
+);
+/* 30 Rechnungen */
+
 
 create table Rechnung(
-                      Rechnung_ID int PRIMARY KEY auto_increment,
-                      Kunden_ID int ,
-                      RechnungsDatum date,
-                      FOREIGN KEY (Kunden_ID) REFERENCES Kunde (Kunde_ID)
-);
-create table Hersteller(
-                         Hersteller_ID int PRIMARY KEY auto_increment ,
-                        Firma varchar(255)
-
+                         Rechnung_ID int PRIMARY KEY auto_increment,
+                         Kunden_ID int ,
+                         RechnungsDatum date,
+                         FOREIGN KEY (Kunden_ID) REFERENCES Kunde (Kunde_ID)
 );
 
-
-
+/* 40 Artikel */
 
 create table Artikel(
-                         Artikel_ID int PRIMARY KEY auto_increment ,
-                         Hersteller_ID int ,
-                         Bezeichnung varchar(255),
-                         Listenpreis decimal,
-                         FOREIGN KEY (Hersteller_ID) REFERENCES Hersteller (Hersteller_ID)
+                        Artikel_ID int PRIMARY KEY auto_increment ,
+                        Hersteller_ID int ,
+                        Bezeichnung varchar(255),
+                        Listenpreis decimal,
+                        FOREIGN KEY (Hersteller_ID) REFERENCES Hersteller (Hersteller_ID)
 );
 
 
+/* 50 Positionen */
 
 
 create table Position(
@@ -39,9 +46,7 @@ create table Position(
                          Artikel_ID int ,
                          Bezeichnung varchar(255),
                          Listepreis decimal,
-                          FOREIGN KEY (Rechnung_ID) REFERENCES Rechnung (Rechnung_ID),
+                         FOREIGN KEY (Rechnung_ID) REFERENCES Rechnung (Rechnung_ID),
                          FOREIGN KEY (Artikel_ID) REFERENCES Artikel (Artikel_ID)
 
 );
-
-
